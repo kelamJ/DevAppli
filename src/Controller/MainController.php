94004a0e95092917e0a61779bf15edc36415b1bb
kelamJ <malekjulien80@gmail.com>
2023-04-25 15:33:59 +0200

    Ajout image

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,20 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Plat;
+use App\Repository\PlatRepository;
 use App\Repository\CategorieRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(CategorieRepository $categorieRepository): Response
+    public function index(CategorieRepository $categorieRepository,
+    PlatRepository $platRepository
+    ): Response
     {
         return $this->render('main/index.html.twig', [
             'categorie' => $categorieRepository->findBy([],
+            ['id' => 'asc']),
+            'plat' => $platRepository->findBy([],
             ['id' => 'asc'])
         ]);
     }
     
+    // #[Route('/{id}', name: 'list')]
+    // public function details(Plat $plat): Response
+    // {
+    //     //On va chercher la liste des plats de la catégorie
+    //     $plats = $plat->getPlat();
+
+    //     return $this->render('categorie/list.html.twig', compact(
+    //     'categorie',
+    //     'plat'));
+    // }
 }
